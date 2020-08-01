431f0f00a5354c2266f7914ca5dc6e71360ef974
Andrew Buter <andrey.buter@gmail.com>
2020-08-01 14:30:08 +0300

    add bundle hash + andd html webpack plugin (simple config)

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	mode: 'development',
-	entry: {
-		main: './src/index.js',
-		nextBundle: './next-script.js'
-	},
-	output: {
-		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'dist')
-	}
-}
+  mode: "development",
+  entry: {
+    main: "./src/index.js",
+    nextBundle: "./src/next-script.js",
+  },
+  output: {
+    // https://webpack.js.org/configuration/output/#outputdevtoolmodulefilenametemplate
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+	  new HTMLWebpackPlugin({
+		  title: 'Test title'
+	  })
+  ]
+};
