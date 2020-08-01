1b6aa0c92039f8cbe83a3a0ec1e3859fac23f5aa
Andrew Buter <andrey.buter@gmail.com>
2020-08-01 14:20:03 +0300

    several named bundles

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@ const path = require('path');
 
 module.exports = {
 	mode: 'development',
-	entry: './src/index.js',
+	entry: {
+		main: './src/index.js',
+		nextBundle: './next-script.js'
+	},
 	output: {
-		filename: 'bundle.js',
+		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist')
 	}
 }
