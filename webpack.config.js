494d6f84c0c2b300e2c1bf8de9f64a71205b83f4
Andrew Buter <andrey.buter@gmail.com>
2020-08-01 14:35:12 +0300

    upd html webpack plugin, add clean plugin, add context prop

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: {
-    main: "./src/index.js",
-    nextBundle: "./src/next-script.js",
-  },
-  output: {
-    // https://webpack.js.org/configuration/output/#outputdevtoolmodulefilenametemplate
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [
-	  new HTMLWebpackPlugin({
-		  title: 'Test title'
-	  })
-  ]
+	context: path.resolve(__dirname, 'src')
+	mode: "development",
+	entry: {
+		main: "./index.js",
+		nextBundle: "./next-script.js",
+	},
+	output: {
+		// https://webpack.js.org/configuration/output/#outputdevtoolmodulefilenametemplate
+		filename: "[name].[contenthash].js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: "./index.html",
+		}),
+		new CleanWebpackPlugin(),
+	],
 };
