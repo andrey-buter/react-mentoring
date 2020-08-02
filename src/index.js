ac9bd3726b6bb312c8e3f212437b2aca1706806a
Andrew Buter <andrey.buter@gmail.com>
2020-08-02 22:38:08 +0300

    update webpack config

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./assets/styles.css";
+import "./assets/styles.scss";
+
+alert(1);
+
+class Test {
+	static id = Date.now()
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,129 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+const TersetWebpackPlugin = require("terser-webpack-plugin");
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
 
 module.exports = {
-	context: path.resolve(__dirname, 'src')
-	mode: "development",
-	entry: {
-		main: "./index.js",
-		nextBundle: "./next-script.js",
-	},
-	output: {
-		// https://webpack.js.org/configuration/output/#outputdevtoolmodulefilenametemplate
-		filename: "[name].[contenthash].js",
-		path: path.resolve(__dirname, "dist"),
-	},
-	plugins: [
-		new HTMLWebpackPlugin({
-			template: "./index.html",
-		}),
-		new CleanWebpackPlugin(),
-	],
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: {
+    main: ["@babel/polyfill", "./index.js"],
+    nextBundle: "./next-script.ts",
+  },
+  output: {
+    // https://webpack.js.org/configuration/output/#outputdevtoolmodulefilenametemplate
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".js", "jsx"],
+    alias: {
+      "@models": path.resolve(__dirname, "src/models"),
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+    minimizer: isDev
+      ? []
+      : [new OptimizeCssAssetsWebpackPlugin(), new TersetWebpackPlugin()],
+  },
+  devServer: {
+    port: 4000,
+    hot: isDev,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./index.html",
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    // new CopyWebpackPlugin([
+    // 	{
+    // 		from: path.resolve(__dirname, 'src/favicon.icon'),
+    // 		to: path.resolve(__dirname, 'dist')
+    // 	}
+    // ]),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hrm: isDev,
+              reloadAll: true,
+            },
+          },
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hrm: isDev,
+              reloadAll: true,
+            },
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-typescript"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
+      },
+      {
+        test: /\.jss$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
+      },
+    ],
+  },
 };
