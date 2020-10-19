e0f62b605f66a42fa5f907b3631e619ee5e07de6
Andrei Butser <andrei_butser@epam.com>
2020-10-19 23:39:06 +0300

    remove comments

--- src/Components/MovieEditForm/MovieEditFormContainer.tsx
+++ src/Components/MovieEditForm/MovieEditFormContainer.tsx
@@ -26,17 +26,11 @@ const MovieEditFormContainer = (props: Props) => {
 			...values
 		}
 		const response = await fetch(url, {
-			method: isUpdate ? 'PUT' : 'POST', // *GET, POST, PUT, DELETE, etc.
-			// mode: 'cors', // no-cors, *cors, same-origin
-			// cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-			// credentials: 'same-origin', // include, *same-origin, omit
+			method: isUpdate ? 'PUT' : 'POST',
 			headers: {
 				'Content-Type': 'application/json'
-				// 'Content-Type': 'application/x-www-form-urlencoded',
 			},
-			// redirect: 'follow', // manual, *follow, error
-			// referrerPolicy: 'no-referrer', // no-referrer, *client
-			body: JSON.stringify(data) // body data type must match "Content-Type" header
+			body: JSON.stringify(data)
 		});
 
 		const responseData = await response.json();
